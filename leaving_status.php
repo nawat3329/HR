bfa79a1958da2835f87dc10ce00e4a19faa51184
Jakkapong Karndee <6222782714@g.siit.tu.ac.th>
2021-11-08 01:28:53 +0700

    updated2?

--- leaving_status.php
+++ leaving_status.php
@@ -14,7 +14,7 @@
 					$s_date = $_POST['date_leave'];
 					$e_date = $_POST['date_leave'];
 					$e_leave = $_POST['emergency_leave'];
-          $file = $_POST['File'];
+          $file = $_POST['file'];
 
 					$q="INSERT INTO leaving form(Form_Title, Form_detail, Form_status, Form_DateStart, Form_DateEnd) VALUES ('$title','$reason','$s_date','$e_date','$e_leave','$file');";
 					$result=$mysqli->query($q);
@@ -24,7 +24,52 @@
 					}
 				}
 			?>
+
+<table>
+                <col width="15%">
+                <col width="30%">
+                <col width="30%">
+                <col width="20%">
+                <col width="5%">
+
+                <tr>
+                    <th>Title</th> 
+                    <th>Reason</th>
+                    <th>Date Start</th>
+                    <th>Date End</th>
+                    <th>Emergency Leave</th>
+                    <th>Attach File</th>
+                </tr>
+ 		 <?php 
+				 	$q="select * from leaving form inner join user on leaving form.user_id = user.user_id";
+					$result=$mysqli->query($q);
+					if(!$result){
+						echo "Select failed. Error: ".$mysqli->error ;
+						return false;
+					}
+				 while($row=$result->fetch_array()){ ?>
+                 <tr>
+                    <td><?=$row['title']?></td> 
+                    <td><?=$row['date_leave']?></td>
+                    <td><?=$row['date_leave']?></td>
+                    <td><?=$row['emergency_leave']?></td>
+					<td>
+            ?>
+					<?php 
+					if ($row['emergency_leave']==1)
+					    echo "<input type='checkbox' CHECKED >";
+					else 
+					    echo "<input type='checkbox' disabled >";
+					} ?>
+
+			<?php $count=$result->num_rows;
+					echo "<tr><td colspan=7 align=right>Total $count records</td></tr>";
+					$result->free();
+			?>
+            </table>
   </div>
+
+  
 
   <button onclick="history.go(-1);">Back </button>
 
