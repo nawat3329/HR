f4712fa31aafdc7a84340d65ac31d34ab0f4b112
Nawat <6222770685@siit.tu.ac.th>
2021-11-08 12:56:46 +0700

    add button permission

--- main.php
+++ main.php
@@ -1,4 +1,10 @@
 <!--- main page after login --->
+<?php 
+  session_start();
+  require('connect.php');
+  $firstname = $_SESSION['firstname'];
+  $user_position = $_SESSION['user_position']
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,13 +14,18 @@
 <body>
 <div class="container">
   <div class="Welcome">
-  <h1>Welcome back!</h1>
+  
+    <h1>Welcome back!</h1>
+  
   </div>
+  
   <!--<div class="Calendar">
     <img src="calendar.png" alt="calendar" class="calendar">
   </div>-->
   <div class="Name">
-  <h3>Name: <h3>
+  <?php
+    echo "<h3>Name: ".$firstname."<h3>";
+  ?>
   <h3>Quota remaining:<h3>
   </div>
   <div class="userfunc">
@@ -29,17 +40,30 @@
   <!--Different button for different position-->
 
   <div class="specialfunc">
+  
     <div class="sf1">
-    <button type="button">Leaving Request</button>
-    <button type="button">User Quota</button>
-    <button type="button">Edit Calendar</button>
+    <?php
+    if ($user_position == "hr"){
+      echo '<h3>HR</h3>';
+    echo '<button type="button">Leaving Request</button>';
+    echo '<button type="button">User Quota</button>';
+    echo '<button type="button">Edit Calendar</button>';
+    }
+    ?>
   </div>
+  
+
     <div class="sf2">
-    <button type="button">Register New Staff</button>
-    <button type="button">List of User</button>
-    <button type="button">Reset All Quota</button>
+    <?php
+    if ($user_position == "admin"){
+    echo '<h3>Admin</h3>';
+    echo '<button type="button">Register New Staff</button>';
+    echo '<button type="button">List of User</button>';
+    echo '<button type="button">Reset All Quota</button>';
+  }
+  ?>
     </div>
-    <div class="sf3">3</div>
+    <div class="sf3"></div>
   </div>
   <div class="logout">
   <button type="button">logout</button>
@@ -47,10 +71,7 @@
   <div class="event-list">
     event-list
   </div>
-  <div class="username">
-    username
-  </div>
-  <div class="quota"></div>
+
 </div>
 </body>
 </html>      
