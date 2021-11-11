4885771d554da496b8a658e3408195d03a9c7d19
Flammableobject <sodseein@gmail.com>
2021-11-11 13:38:23 +0700

    Add register adminedit  user quota list  user list

---
+++ admin_edit_update.php
@@ -0,0 +1,35 @@
+
+		<?php
+        require_once('connect.php'); 
+        if(isset($_POST['su'])) {
+		// Taking all 5 values from the form data(input)
+        $User_ID = $_POST['User_ID'];
+		$User_Position = $_POST['User_Position'];
+		$User_Department = $_POST['User_Department'];
+		$User_Gender = $_POST['User_Gender'];
+        $User_DOB = $_POST['User_DOB'];
+        $User_Tel = $_POST['User_Tel'];
+        $User_Email = $_POST['User_Email'];
+        $User_Address = $_POST['User_Address'];
+        $User_Quota = $_POST['User_Quota'];
+        $User_Username = $_POST['User_Username'];
+        $User_Password = $_POST['User_Password'];
+		
+		// Performing update query execution
+		// here our table name is user
+
+		$sql = "UPDATE user SET  User_Position = '$User_Position', User_Department = '$User_Department', User_Gender = '$User_Gender', User_DOB = '$User_DOB',
+        User_Tel = '$User_Tel',User_Email = '$User_Email',User_Address = '$User_Address',User_Quota = '$User_Quota',
+        User_Username = '$User_Username',User_Password = '$User_Password' WHERE User_ID = '$User_ID'";
+		
+		$result1=$mysqli->query($sql);
+		if(!$result1){
+			echo "UPDATE failed. Error: ".$mysqli->error ;
+			return false;
+			}
+		header("Location: admin_edit_profile.php");
+		}
+		
+		
+		?>
+	
