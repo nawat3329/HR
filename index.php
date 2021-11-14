f14134726d87e06b6115c180d50aed3b98076089
Jakkapong Karndee <6222782714@g.siit.tu.ac.th>
2021-11-14 23:05:06 +0700

    login index session

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include('login.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+header("location: main.php"); // Redirecting To Profile Page
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" href="default.css">
+</head>
+<body>
+
+    <form action="login.php" method="post">
+        <div class="imgcontainer">
+          <img src="logo.jpg" alt="logo" class="logo">
+        </div>
+      
+        <div class="container">
+            <h2><label for="uname"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" name="uname" required></h2>
+      
+        <h2><label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required></h2>
+
+                <button type = "submit" class ="btn btn-primary" >Login</button>
+        </div>
+      
+        <div class="containerlogin" style="background-color:#f1f1f1">
+    
+        </div>
+      </form>
+</body>
+</html> 
--- login.php
+++ login.php
@@ -1,38 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Login</title>
-<link rel="stylesheet" href="default.css">
-</head>
-<body>
 
-    <form action="login.php" method="post">
-        <div class="imgcontainer">
-          <img src="logo.jpg" alt="logo" class="logo">
-        </div>
-      
-        <div class="container">
-            <h2><label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="uname" required></h2>
-      
-        <h2><label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required></h2>
-
-                <button type = "submit" class ="btn btn-primary" >Login</button>
-        </div>
-      
-        <div class="containerlogin" style="background-color:#f1f1f1">
-          <!--<button type="button" class="cancelbtn">Cancel</button>-->
-    
-        </div>
-      </form>
-</body>
-</html>      
-<!--<link rel="stylesheet" href="styles.css">-->
-
-<?php session_start(); require_once('connect.php'); ?>
-<?php 
-	if(isset($_SESSION['login_user'])) {
-		header("location: main.php");
-	}
+<?php
+session_start(); // Starting Session
+$error = ''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else{
+// Define $username and $password
+$username = $_POST['username'];
+$password = $_POST['password'];
+// mysqli_connect() function opens a new connection to the MySQL server.
+$conn = mysqli_connect("localhost", "root", "", "HR");
+// SQL query to fetch information of registerd users and finds user match.
+$query = "SELECT User_username, User_password from User where username=? AND password=? LIMIT 1";
+// To protect MySQL injection for Security purpose
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+$stmt->bind_result($username, $password);
+$stmt->store_result();
+if($stmt->fetch()) //fetching the contents of the row {
+$_SESSION['login_user'] = $username; // Initializing Session
+header("location: main.php"); // Redirecting To Profile Page
+}
+$conn->close(); // Closing Connection
+}
 ?>
